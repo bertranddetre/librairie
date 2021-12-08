cedd6cb5763d3773ecb14657d3ee4fbe79831c46
bertranddetre <detrebertrand@gmail.com>
2021-12-08 23:56:35 +0100

    javascript

---
+++ style.js
@@ -0,0 +1,10 @@
+//créer un livre
+let book;
+let title = "les Misérables";
+let author = "Victor Hugo";
+let price = 14.95;
+let category = "Drame";
+let releaseDate = new Date ("1997,05,23");
+let nbSales = 345;
+
+
